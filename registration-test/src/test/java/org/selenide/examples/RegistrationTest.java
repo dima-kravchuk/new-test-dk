e67fa0a62d48ba8b7c8de3b8a0750f307afa4ed7
dima-kravchuk <dmytro.kravchuk@gmail.com>
2021-11-30 21:45:47 +0200

    RegistrationTest

--- registration-test/src/test/java/org/selenide/examples/RegistrationTest.java
+++ registration-test/src/test/java/org/selenide/examples/RegistrationTest.java
@@ -14,8 +14,20 @@ public class RegistrationTest {
         open("http://automationpractice.com/index.php");
         $(byText("Sign in")).click();
 
-        $(By.name("email_create")).setValue("new_test1@test.com");
+        $(By.name("email_create")).setValue("new_test1111111111@test.com");
         $(By.name("SubmitCreate")).click();
 
+        $(By.name("customer_firstname")).setValue("new_test1");
+        $(By.name("customer_lastname")).setValue("lastname");
+        $(By.name("passwd")).setValue("12345");
+        $(By.name("address1")).setValue("Kyiv");
+        $(By.name("city")).setValue("Kyiv");
+        $(By.name("id_state")).selectOptionByValue("1");
+        $(By.name("postcode")).setValue("54321");
+        $(By.name("id_country")).selectOptionByValue("21");
+        $(By.name("phone_mobile")).setValue("380671234567");
+        $(By.name("alias")).setValue("Kyiv");
+
+        $(By.name("submitAccount")).click();
     }
 }
